ce5002edaafecfcee7b38e285b7a2fd9ca7efad0
xuanssss <kuangyuxuan01@gmail.com>
2024-11-06 12:47:30 +0800

    added enum

--- ClassFiles/oop/PrescribedMedication.java
+++ ClassFiles/oop/PrescribedMedication.java
@@ -2,12 +2,12 @@ package oop;
 public class PrescribedMedication {
     public String name;
     public int numberOfUnits;
-    public String status;
+    public StatusOfPrescribedMedication status;
 
     public PrescribedMedication(String name, int numberOfUnits)
     {
         this.name = name;
         this.numberOfUnits = numberOfUnits;
-        this.status = "Pending";
+        this.status = StatusOfPrescribedMedication.Pending;
     }
 }
